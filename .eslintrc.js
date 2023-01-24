3a2b12b77eb94a6bf7c667617974ed2035c997ac
ivnteterin <spy930712@gmail.com>
2023-01-25 01:53:48 +0200

    feat: add files

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: ['error', 'space'],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
